a130f5d8cd105aede0bb42daf0f3e3a6b3b13c0d
YIHAN <105974692+neil0x46dc@users.noreply.github.com>
2023-04-14 10:30:34 +0900

    chore: clean up

--- mint-nfts-on-layer-2/utils/config.ts
+++ mint-nfts-on-layer-2/utils/config.ts
@@ -1,19 +1,18 @@
-
 const isVercel = process.env.IS_VERCEL !== "1";
 let reddio;
 const initReddio = (client: any) => {
-    try {
-        // ref: https://github.com/vercel/next.js/discussions/13417
-        import('@reddio.com/js').then((mudule) => {
-            reddio = mudule.Reddio;
-            new mudule.Reddio({
-                env: isVercel ? "main" : "test",
-                wagmiClient: client,
-            })
-        });
-    } catch (e) {
-        console.log('@reddio.com/js loaded error')
-    }
+  try {
+    // ref: https://github.com/vercel/next.js/discussions/13417
+    import("@reddio.com/js").then((module) => {
+      reddio = module.Reddio;
+      new reddio({
+        env: isVercel ? "main" : "test",
+        wagmiClient: client,
+      });
+    });
+  } catch (e) {
+    console.log("@reddio.com/js loaded error");
+  }
 };
 
 export { initReddio, reddio, isVercel };
